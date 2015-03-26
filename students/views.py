88fdda6e75e3cc1d9735b5660ed2b8794917d4d6
Mikhail Podanev <mpodanev@gmail.com>
2015-03-26 21:21:17 +0200

    add templates app to our project

--- students/views.py
+++ students/views.py
@@ -41,7 +41,7 @@ def students_delete(request, sid):
 # Views for Groups
 
 def groups_list(request):
-    return HttpResponse('<h1>Groups Listing</h1>')
+    return render(request, 'groups/groups_list.html', {})
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
